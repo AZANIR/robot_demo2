15060d85c7a609eaeb671cb55622fdb39b2923da
AZANIR <eleoneks@gmail.com>
2022-10-03 22:42:41 +0300

    sdfgsdfgsdfg

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from mgs_client import AuthenticatedClient
-from mgs_client.models import CallProfileConfiguration
-from mgs_client.api.call_generation_profile_rest_controller import get_all_call_profiles
-from mgs_client.types import Response
+# Rename `os.environ` to `env` for clarity
+from os import environ as env
+from dotenv import load_dotenv
 
-from config.default import MGS_URL, CHE_URL, TOKEN
+load_dotenv()
 
-client = AuthenticatedClient(base_url=MGS_URL, token=TOKEN)
 
-response: Response[CallProfileConfiguration] = get_all_call_profiles.sync_detailed(client=client)
-print(response.content)
+def test_secret():
+    item = env.get('CLIENT_SECRET')
+    print(item)
+    print("CLIENT_SECRET: {}".format(env["CLIENT_SECRET"]))
