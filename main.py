9b2acd4240f79e0f98756224c1028f2009623764
AZANIR <eleoneks@gmail.com>
2022-10-03 23:09:37 +0300

    sdfgsdfgsdfg

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 from os import environ as env
 from dotenv import load_dotenv
 
-load_dotenv()
-
 
 def test_secret():
+    load_dotenv()
     item = env.get('CLIENT_SECRET')
     print(item)
-    print("CLIENT_SECRET: {}".format(env["CLIENT_SECRET"]))
+    print("CLIENT_SECRET: {}".format(item if item != None else env.get('CLIENT_SECRET')))
+
+
+test_secret()
