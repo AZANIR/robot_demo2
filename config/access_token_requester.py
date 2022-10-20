0fb76ac81a2208f4e1ec7c4ce7d66d2d5f9544d5
Leonid Mayevskyi (SOFTSERVE) <Leonid.Mayevskyi@motorolasolutions.com>
2022-10-20 16:32:39 +0300

    update44

--- config/access_token_requester.py
+++ config/access_token_requester.py
@@ -1,8 +1,8 @@
-import requests
 # Rename `os.environ` to `env` for clarity
 from os import environ as env
-from dotenv import load_dotenv
+import requests
 from config.default import CLIENT_ID, TOKEN_URL, SCOPE, GRANT_TYPE
+from dotenv import load_dotenv
 
 
 def request_access_token():
@@ -10,4 +10,5 @@ def request_access_token():
     client_secret = env.get('CLIENT_SECRET')
     json_body = {"grant_type": GRANT_TYPE, "scope": SCOPE, "client_id": CLIENT_ID, "client_secret": client_secret}
     response = requests.post(TOKEN_URL, data=json_body)
+    print(response)
     return response.json()['access_token']
