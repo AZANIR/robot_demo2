945bf7eac5ffe0b827e46b672c86310aff89e0db
Leonid Mayevskyi (SOFTSERVE) <Leonid.Mayevskyi@motorolasolutions.com>
2022-10-21 12:40:20 +0300

    update7

--- setup_vpn.sh
+++ setup_vpn.sh
@@ -4,7 +4,8 @@ sudo su
 apt-get update
 apt-get install ppp net-tools -y
 cd $HOME
-cd opt
+cd opt/
+ls -a
 #cd ../../../../../opt/
 
 #wget http://cdn.software-mirrors.com/forticlientsslvpn_linux_4.4.2328.tar.gz
