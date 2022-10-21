37f51531c3b196c419f1d9b71d524f088e947b91
Leonid Mayevskyi (SOFTSERVE) <Leonid.Mayevskyi@motorolasolutions.com>
2022-10-21 13:09:39 +0300

    update7

--- setup_vpn.sh
+++ setup_vpn.sh
@@ -1,20 +1,10 @@
 sudo apt-get install wget curl expect -y
 sudo apt-get update
 sudo apt-get install ppp net-tools -y
-cd ../../../../../
-ls
-
-#wget http://cdn.software-mirrors.com/forticlientsslvpn_linux_4.4.2328.tar.gz
-#tar zxvf forticlientsslvpn_linux_4.4.2328.tar.gz
-uname -m
-#cd forticlientsslvpn/64bit/
 #sudo apt-get install ppp iproute2 -y
-#cp ./setup.linux.sh ./forticlientsslvpn/64bit/helper/
-#sudo chown root:root ./setup.linux.sh
-#sudo chmod 600 ./setup.linux.sh
-#sudo chmod +x ./setup.linux.sh
-#./forticlientsslvpn/64bit/helper/setup.linux.sh -Y
-#sudo chown root:root ./forti-vpn.sh
-#sudo chmod 600 ./forti-vpn.sh
-#sudo chmod +x ./forti-vpn.sh
-#sudo ./forti-vpn.sh &
+wget http://cdn.software-mirrors.com/forticlientsslvpn_linux_4.4.2328.tar.gz
+tar zxvf forticlientsslvpn_linux_4.4.2328.tar.gz
+sudo chown root:root ./forti-vpn.sh
+sudo chmod 600 ./forti-vpn.sh
+sudo chmod +x ./forti-vpn.sh
+sudo ./forti-vpn.sh &
