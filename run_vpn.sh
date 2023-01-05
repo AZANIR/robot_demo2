c8168847042e68bed70899ded74beb195b2007dd
Leonid Mayevskyi (SOFTSERVE) <Leonid.Mayevskyi@motorolasolutions.com>
2023-01-05 16:02:28 +0200

    update4

--- run_vpn.sh
+++ run_vpn.sh
@@ -1,4 +1,4 @@
-sudo openfortivpn 65.229.28.150:4443 -u lmayevskyi --trusted-cert d04a0c9d55c42e37f83637af03e339b3dcf325cfa5a4f506b84ba31f744342a9
+sudo ./forticlientsslvpn/64bit/forticlientsslvpn_cli 65.229.28.150:4443 -u lmayevskyi --trusted-cert d04a0c9d55c42e37f83637af03e339b3dcf325cfa5a4f506b84ba31f744342a9
 expect "Password for VPN:"
 send "ct}DH>!3jW~8R*2<\r"
 #expect "*(Y/N)"
